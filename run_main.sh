d8129fe54014437643def835ed3b51e8f1398196
Doorn <r.v.doorn1@student.tue.nl>
2024-03-09 11:37:07 +0100

    first test to see how long an epoch takes

--- run_main.sh
+++ run_main.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
-#SBATCH --time=0:20:00
+#SBATCH --time=0:30:00
 
 cd /gpfs/home6/scur0756/Final_Assignment/5LSM0-final-project-Roy-van-Doorn/
 mkdir wandb/$SLURM_JOBID
