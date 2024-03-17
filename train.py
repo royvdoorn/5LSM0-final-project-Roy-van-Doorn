904d3e0804656e3482d3950833cd48087defac32
Doorn <r.v.doorn1@student.tue.nl>
2024-03-17 15:36:30 +0100

    Training new model

--- train.py
+++ train.py
@@ -55,8 +55,8 @@ def main(args):
 
     # define transform
     regular_transform = transforms.Compose([transforms.Resize((256, 256)),
-                                            transforms.RandomHorizontalFlip([0.5]),
-                                            transforms.RandomVerticalFlip([0.5]),
+                                            transforms.RandomHorizontalFlip(p=0.5),
+                                            transforms.RandomVerticalFlip(p=0.5),
                                             transforms.ToTensor(),
                                             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
