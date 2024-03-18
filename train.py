5822df9d2ff23fe1f676335a09dd3a1ca7fb99a0
Doorn <r.v.doorn1@student.tue.nl>
2024-03-18 15:24:40 +0100

    training new model

--- train.py
+++ train.py
@@ -178,5 +178,5 @@ if __name__ == "__main__":
     # Get the arguments
     parser = get_arg_parser()
     args = parser.parse_args()
-    #main(args)
-    visualize()
+    main(args)
+    #visualize()
