915178108d9c0c8218bfb9ac9e41b4abcbfc4483
Doorn <r.v.doorn1@student.tue.nl>
2024-03-20 11:01:49 +0100

    Training new model

--- train.py
+++ train.py
@@ -89,7 +89,7 @@ def main(args):
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 0.9)
 
     # training/validation loop
-    epochs = 25
+    epochs = 20
 
     train_loss = []
     val_loss = []
