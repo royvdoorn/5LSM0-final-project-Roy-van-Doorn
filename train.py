3b477908534a7e46de9ddbc56583ce4e6bf4c28a
Doorn <r.v.doorn1@student.tue.nl>
2024-03-21 16:13:11 +0100

    Training new model

--- train.py
+++ train.py
@@ -91,7 +91,7 @@ def main(args):
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 0.9)
 
     # training/validation loop
-    epochs = 2
+    epochs = 25
 
     train_loss = []
     val_loss = []
@@ -160,7 +160,7 @@ def visualize():
     model.load_state_dict(torch.load("models\\efficiency_model_data_aug"))
 
     # define transform
-    regular_transform = transforms.Compose([transforms.Resize((256, 256)),
+    regular_transform = transforms.Compose([transforms.Resize((270, 270)),
                                             transforms.ToTensor(),
                                             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
     
